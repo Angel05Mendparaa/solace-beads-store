acc404a155bfefd6cb2f17403a4b153f7d9e9f8d
Angel05Mendpara <mendparaangelemail@gmail.com>
2026-07-07 16:14:46 +0530

    Add Hompage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Hero from './components/Hero/Hero.jsx'
-import InfiniteMarquee from './components/InfiniteMarquee/InfiniteMarquee.jsx'
-import FeaturedCollections from './components/FeaturedCollections/FeaturedCollections.jsx'
-import AboutSection from './components/AboutSection/AboutSection.jsx'
-import CTASection from './components/CTASection/CTASection.jsx'
-import Footer from './components/Footer/Footer.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <InfiniteMarquee />
-      <FeaturedCollections />
-      <AboutSection />
-      <CTASection />
-      <Footer />
+      <Home />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar.jsx';
+import Hero from '../components/Hero/Hero.jsx';
+import InfiniteMarquee from '../components/InfiniteMarquee/InfiniteMarquee.jsx';
+import FeaturedCollections from '../components/FeaturedCollections/FeaturedCollections.jsx';
+import AboutSection from '../components/AboutSection/AboutSection.jsx';
+import CTASection from '../components/CTASection/CTASection.jsx';
+import Footer from '../components/Footer/Footer.jsx';
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+      <Hero />
+      <InfiniteMarquee />
+      <FeaturedCollections />
+      <AboutSection />
+      <CTASection />
+      <Footer />
+    </div>
   )
 }
 
